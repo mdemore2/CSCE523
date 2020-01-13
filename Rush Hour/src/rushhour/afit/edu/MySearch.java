95662b50c464a4dd7b5c30dc53c84d05d4a06a9e
mdemore2 <mark.demore2@gmail.com>
2020-01-13 08:33:23 -0500

    added rush hour files, created mysearch class

---
+++ Rush Hour/src/rushhour/afit/edu/MySearch.java
@@ -0,0 +1,19 @@
+package rushhour.afit.edu;
+
+public class MySearch implements Search {
+
+	@Override
+	public Move findMoves() {
+		// TODO Auto-generated method stub
+		return null;
+		//return move list
+	}
+
+	@Override
+	public long nodeCount() {
+		// TODO Auto-generated method stub
+		return 0;
+		//return count of nodes (aka moves) made
+	}
+
+}
